76ce3022ccbb2a3988cd432795681d62a777c24b
Mauro Amoedo <mauro.amoedo@itscordoba.edu.ar>
2025-06-17 01:37:23 -0300

    formulario armado, muestra textarea cuando selecciono OTRO

---
+++ script.js
@@ -0,0 +1,27 @@
+// Espera a que cargue el doc index
+document.addEventListener("DOMContentLoaded", function () {
+  const select = document.getElementById("como");
+  const otroDiv = document.getElementById("otro-especificar");
+  const form = document.getElementById("formulario-contacto");
+
+  // funcion de "otro"
+  select.addEventListener("change", function () {
+    if (select.value === "otro") {
+      // al elegir "otro", muestra la textarea
+      otroDiv.style.display = "block";
+    } else {
+      // Al elegir las otras, se oculta la textarea
+      otroDiv.style.display = "none";
+    }
+  });
+
+  // Envio de formulario
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    alert("Gracias por contactarte con nosotros, en 48 hs hábiles recibiras respuesta :)");
+
+    form.reset(); 
+    otroDiv.style.display = "none"; // Oculta el textarea que estaba visible
+  });
+});
